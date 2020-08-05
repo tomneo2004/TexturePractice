cc4caa1ed33956d6e99327c81c53bf4c0f4d1de2
Nelson <tomneo2004@gmail.com>
2020-08-05 17:24:42 +0800

    init

---
+++ TexturePractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TexturePractice
+//
+//  Created by Nelson on 5/8/20.
+//  Copyright © 2020 Nelson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
